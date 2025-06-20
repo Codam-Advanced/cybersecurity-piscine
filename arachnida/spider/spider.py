001ea6c7063ee5b2bb73d1040a66bf60c9736ba0
Jisse Meruma <jissemeruma@gmail.com>
2025-06-20 16:28:33 +0200

    Looks good now

--- arachnida/spider/spider.py
+++ arachnida/spider/spider.py
@@ -24,7 +24,7 @@ def parse_arguments():
     return args
 
 def find_tag_and_attribute(html: str, tag: str, attribute: str, start_index: int):
-   start_index = html.find("<" + tag, start_index)
+   start_index = html.find("<" + tag + " ", start_index)
    if (start_index == -1):
     return "", 0
    start_index = html.find(attribute + "=", start_index)
@@ -33,11 +33,11 @@ def find_tag_and_attribute(html: str, tag: str, attribute: str, start_index: int
    temp_index = html.find(" ", start_index, end_index)
    if (temp_index != -1):
       end_index = temp_index
-   # print(html[start_index:end_index].strip("\""))
    return html[start_index:end_index].strip("\""), start_index
 
 def change_url(url: ParseResult, img_url: str):
-   img_url = img_url.strip("../")
+   if (img_url.find("../") != -1):
+      img_url = img_url.strip("../")
    if (img_url[0] != "/"):
       img_url = "/" + img_url
    img_url = url.scheme + "://" + url.hostname + img_url
@@ -53,7 +53,9 @@ def scrape_page(url: ParseResult, html: str):
     if (img_url.find("http") == -1):
       img_url = change_url(url, img_url)
     upload_file_path = args.path + img_url[img_url.rfind("/") + 1:]
-    if (os.path.isfile(upload_file_path)):
+    if (upload_file_path.find("?") != -1):
+       upload_file_path = upload_file_path[:upload_file_path.find("?")]
+    if (os.path.isfile(upload_file_path) or img_url.endswith((".jpg", ".jpeg", ".png", ".gif" ".bmp")) == False):
        continue
     with open(upload_file_path, "wb") as file:
         page = urlopen(img_url)
@@ -62,9 +64,9 @@ def scrape_page(url: ParseResult, html: str):
 miep = set()
 
 def crawl_page(url: str, length: int):
-   
+   print("Length = " + str(length), url) 
    miep.add(url)
-   print(url)
+   print(miep)
    parsed_url = urlparse(url)
    try:
       page = urlopen(parsed_url.geturl())
@@ -75,21 +77,23 @@ def crawl_page(url: str, length: int):
          a_url, start_index = find_tag_and_attribute(html, "a", "href", start_index)
          if (a_url == ""):
             break
-         print("before " + a_url) 
          if (a_url.find("http") == -1):   
             a_url = change_url(parsed_url, a_url)
-         print("after " + a_url) 
-         if (length > 0 and a_url not in miep): 
+         if (a_url in miep):
+            continue
+         if (length > 0 and a_url.endswith(".zip") == False): 
             crawl_page(a_url, length - 1)
-         scrape_page(parsed_url, html)
+      scrape_page(parsed_url, html)
    except HTTPError as e:
-      if e.code == 308:
+      if e.code == 308 or e.code == 302:
          redirected_url = e.headers["location"]
          print(f"Redirected to {redirected_url}")
          if (length > 0 and a_url not in miep): 
             crawl_page(redirected_url, length)
+      if e.code == 404:
+         print(f"Site Not Found! {url}")
       else:
-         raise
+         print(f"Error {e.code} found on site {url}")
 
 if __name__ == "__main__":
    args = parse_arguments()
