e9bd83e81272f5ce57986804bb2391960ce13018
Jisse Meruma <jissemeruma@gmail.com>
2025-07-03 16:40:40 +0200

    Scorpian was easier than i thought

--- arachnida/scorpion/scorpion.py
+++ arachnida/scorpion/scorpion.py
@@ -1,14 +1,30 @@
 import argparse
 import sys
-from PIL import Image
-
+from PIL import Image, ExifTags
 
 def extract_file_data(files: list[str]):
     for file in files:
-        print(file)
-        image = (Image.open(file))
-        exif_data = image.getexif()
-        print(exif_data.pop())        
+        image = Image.open(file)
+        exif_data = image.getexif().items()
+        print(f"|               The Image - [{image.filename}] -            |")
+        print(f"| The Image Format	    - [{image.format}]	              |")
+        print(f"| The Image Size	    - W [{image.width}] H [{image.height}]	      |")
+        for key, val in exif_data:
+            if ExifTags.TAGS[key] != "DateTime":
+                        continue
+            print(f"| The Image Creation date   - [{val}]   |")
+        print("\n[EXIF_DATA]:")
+        if not exif_data:
+            print("Sorry, image has no exif data.")
+        else:
+            for key, val in exif_data:
+                if key in ExifTags.TAGS:
+                    if ExifTags.TAGS[key] == "DateTime":
+                        continue
+                    print(f"[{ExifTags.TAGS[key]}]:({val})")
+                else:
+                    print(f"[{key}]:({val})")
+        print("---------------------------")
                 
 
 if "__main__" == __name__:
